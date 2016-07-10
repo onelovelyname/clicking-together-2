44da667186e7390ea406a2ccc5a877d0639dc9d1
Melanie Gin <mgin@etsy.com>
2016-07-10 16:13:15 -0700

    Update camp2016 and navigation bar

--- routes/views/camp2016.js
+++ routes/views/camp2016.js
@@ -1,9 +1,15 @@
-var keystone = require('keystone');
+var keystone = require('keystone'),
+    Project = keystone.list('Project');
  
 exports = module.exports = function(req, res) {
     
     var view = new keystone.View(req, res);
     
-    view.render('camp2016');
+    Project.model.find()
+    .where('year', 2016)
+    .populate('student cover')
+    .exec(function(error, projects) {
+      view.render('camp2016', { projects: projects });
+    });
     
 }
