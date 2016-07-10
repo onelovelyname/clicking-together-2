0f74cc8dbc1b37554deaa132014a701b71f5dddc
Melanie Gin <mgin@etsy.com>
2016-07-09 20:37:38 -0700

    Add photo field to students model

--- models/students.js
+++ models/students.js
@@ -6,6 +6,7 @@ var Student = new keystone.List('Student');
 Student.add({
     name: { type: String, initial: true, required: true, index: true },
     school: { type: String, initial: true },
+    photo: { type: String, initial: true }
 });
  
 Student.register();
