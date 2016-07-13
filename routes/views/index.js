e36af26e9227ee62888a15047f1493085f5fb6f2
Melanie Gin <mgin@etsy.com>
2016-07-13 00:36:47 -0700

    playing with cover

--- routes/views/index.js
+++ routes/views/index.js
@@ -1,9 +1,14 @@
-var keystone = require('keystone');
+var keystone = require('keystone'),
+  Student = keystone.list('Student');
  
 exports = module.exports = function(req, res) {
     
     var view = new keystone.View(req, res);
-    
-    view.render('index');
-    
+
+    Student.model.find()
+    .exec(function(error, students) {
+      console.log("students: ", students); 
+      view.render('index', {students: students});
+    });
+        
 }
