057521da788e98b6ab31502c66f22b1e4b22d6bc
Melanie Gin <mgin@etsy.com>
2016-07-14 20:43:51 -0700

    Add cover photo, style projects preview, style cover

--- public/js/popup.js
+++ public/js/popup.js
@@ -1,6 +1,24 @@
-$('.parent-photos').magnificPopup({
-  delegate: 'a', // child items selector, by clicking on it popup will open
-  type: 'image',
-  gallery: {enabled: true},
-  titleSrc: 'title'
+$(document).ready(function() {
+
+  $('.parent-photos').magnificPopup({
+    delegate: 'a', // child items selector, by clicking on it popup will open
+    type: 'image',
+    gallery: {enabled: true},
+    titleSrc: 'title'
+  });
+
+  $('.link-preview').on('hover', function() {
+
+  });
+
+
+$('.link-preview').hover(
+  function () {
+    $(this).addClass("hover");
+  },
+  function () {
+    $(this).removeClass("hover");
+  }
+);
+
 });
