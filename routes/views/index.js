f173965ffdc95532a568a2ba1a967720c2be8b13
Melanie Gin <mgin@etsy.com>
2016-07-28 22:30:49 -0700

    Remove unnecessary index code

--- routes/views/index.js
+++ routes/views/index.js
@@ -2,13 +2,6 @@ var keystone = require('keystone'),
   Student = keystone.list('Student');
  
 exports = module.exports = function(req, res) {
-    
     var view = new keystone.View(req, res);
-
-    Student.model.find()
-    .exec(function(error, students) {
-      console.log("students: ", students); 
-      view.render('index', {students: students});
-    });
-        
-}
+    view.render('index');
+};
